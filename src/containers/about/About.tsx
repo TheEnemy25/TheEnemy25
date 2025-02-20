bfe7dbc83ccd9854435c8e88aba48077a97eab9a
TheEnemy25 <andrikkobrin@gmail.com>
2025-02-20 19:28:01 +0200

    style(About): Added max-width and centered layout

--- src/containers/about/About.tsx
+++ src/containers/about/About.tsx
@@ -8,45 +8,41 @@ const About: React.FC = () => {
     const currentTime = useCurrentTime("Europe/Kyiv");
 
     return (
-        <section className="about">
-            <article className="about__wrapper">
-                <div className="about__info">
-                    <div className="about__header">
-                        <h1 className="about__header-title">Andrian Kobryn</h1>
-                        <h2 className="about__header-subtitle">.NET Developer</h2>
+        <>
+            <section className="about">
+                <article className="about__wrapper">
+                    <div className="about__info">
+                        <div className="about__header">
+                            <h1 className="about__header-title">Andrian Kobryn</h1>
+                            <h2 className="about__header-subtitle">.NET Developer</h2>
+                        </div>
+
+                        <div className="about__social">
+                            {contactData.map((item, index) => (
+                                <div key={index} className="about__social-contact">
+                                    <span className="about__social-contact-icon">{item.icon}</span>
+                                    {item.text === "Ukraine, Lviv" ? (
+                                        <p className="about__social-contact-text">
+                                            Ukraine, Lviv | <time dateTime={new Date().toISOString()} className="time-text">{currentTime}</time>
+                                        </p>
+                                    ) : item.link ? (
+                                        <a href={item.link} className="about__social-contact-link" target="_blank" rel="noopener noreferrer">
+                                            {item.text}
+                                        </a>
+                                    ) : (
+                                        <p className="about__social-contact-text">{item.text}</p>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
                     </div>
 
-                    <div className="about__social">
-                        {contactData.map((item, index) => (
-                            <div key={index} className="about__social-contact">
-                                <span className="about__social-contact-icon">{item.icon}</span>
-                                {item.text === "Ukraine, Lviv" ? (
-                                    <p className="about__social-contact-text">
-                                        Ukraine, Lviv | <time dateTime={new Date().toISOString()} className="time-text">{currentTime}</time>
-                                    </p>
-                                ) : item.link ? (
-                                    <a href={item.link} className="about__social-contact-link" target="_blank" rel="noopener noreferrer">
-                                        {item.text}
-                                    </a>
-                                ) : (
-                                    <p className="about__social-contact-text">{item.text}</p>
-                                )}
-                            </div>
-                        ))}
+                    <div className="about__photo">
+                        <img src={photo} alt="Andrian Kobryn" className="about__photo-img" />
                     </div>
-                </div>
-
-                <div className="about__photo">
-                    <img src={photo} alt="Andrian Kobryn" className="about__photo-img" />
-                </div>
-            </article>
-
-            <article className="about__wrapper">
-                <div className="about__description">
-                    фівпніфнгевфіневіфе
-                </div>
-            </article>
-        </section>
+                </article>
+            </section>
+        </>
     );
 };
 
