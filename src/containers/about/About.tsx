7786b99f35a6da15ac0b4ae9e426a2a041f510e0
TheEnemy25 <andrikkobrin@gmail.com>
2025-02-24 19:33:55 +0200

    feat(About): Added image, summary blocks

--- src/containers/about/About.tsx
+++ src/containers/about/About.tsx
@@ -7,20 +7,25 @@ const About: React.FC = () => {
     return (
         <>
             <section className="about">
-                <article className="about__wrapper">
+                <article className="about-container">
+                    <header className="about-container__header">
+                        <h1 className="about-container__header-title">Andrian Kobryn</h1>
+                        <h2 className="about-container__header-subtitle">.NET Developer</h2>
+                    </header>
 
-                    <div className="about__info">
-                        <div className="about__header">
-                            <h1 className="about__header-title">Andrian Kobryn</h1>
-                            <h2 className="about__header-subtitle">.NET Developer</h2>
-                        </div>
+                    <div className="about-container__info">
+                        <h2 className="about-container__info-title">Summary</h2>
+                        <p className="about-container__info-text">Entry-level back-end developer with practical knowledge of C#, ASP.NET Core, EF
+                            Core, MS SQL, and foundational front-end skills in HTML, CSS, React, Next.js, and
+                            Tailwind. Passionate about building efficient and scalable solutions that meet modern
+                            development standards. Committed to continuous professional growth and eager to
+                            apply my knowledge to solve real-world challenges as part of a team of professionals</p>
                     </div>
-
-                    <div className="about__photo">
-                        <img src={photo} alt="Andrian Kobryn" className="about__photo-img" />
-                    </div>
-
                 </article>
+
+                <figure className="about__photo">
+                    <img src={photo} alt="Andrian Kobryn" className="about__photo-img" />
+                </figure>
             </section>
         </>
     );
