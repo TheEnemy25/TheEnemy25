ec3ad03427b6f6cca49eb1b27eda74072001e019
TheEnemy25 <andrikkobrin@gmail.com>
2025-02-24 19:31:18 +0200

    refactor(Header): Removed unnecessary block

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,34 +6,32 @@ const Header: React.FC = () => {
     return (
         <>
             <header className="header">
-                <div className="header__container">
-                    <Link to="/" className='header__logo'>
-                        Portfolio
-                    </Link>
+                <Link to="/" className='header__logo'>
+                    Portfolio
+                </Link>
 
-                    <nav className="header__nav">
-                        <ul className="header__nav-list">
-                            <li>
-                                <Link to="/movie/movies-list">Stack</Link>
-                            </li>
-                            <li>
-                                <Link to="/cinema-theater/CinemaTheater">Experience</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                <nav className="header__nav">
+                    <ul className="header__nav-list">
+                        <li>
+                            <Link to="/movie/movies-list">Stack</Link>
+                        </li>
+                        <li>
+                            <Link to="/cinema-theater/CinemaTheater">Experience</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-                    <div className="header__controls">
-                        <div className="language-switch">
-                            <button className="language-switch__btn">EN</button>
-                            <span className='language-switch__stick'>|</span>
-                            <button className="language-switch__btn">UA</button>
-                        </div>
+                <div className="header__controls">
+                    <div className="language-switch">
+                        <button className="language-switch__btn">EN</button>
+                        <span className='language-switch__stick'>|</span>
+                        <button className="language-switch__btn">UA</button>
+                    </div>
 
-                        {/* <div className="theme-switch">
+                    {/* <div className="theme-switch">
                             <button className="theme-switch__btn">🌙</button>
                             <button className="theme-switch__btn">☀️</button>
                         </div> */}
-                    </div>
                 </div>
             </header>
         </>
