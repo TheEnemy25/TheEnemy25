9d1f80202fed0f0b2547e7502d3df2f6ca458e04
TheEnemy25 <andrikkobrin@gmail.com>
2025-02-28 19:15:57 +0200

    styles(project): Replaced px to rem

--- src/components/contacts-panel/ContactsPanel.tsx
+++ src/components/contacts-panel/ContactsPanel.tsx
@@ -1,38 +1,51 @@
 import "./ContactsPanel.scss";
-import { contactData } from "../../data/contactData";
 import { useCurrentTime } from "../../hooks/useCurrentTime";
+import { ContactTypes } from "../../types/contactTypes";
+import { contactsData } from "../../data/contactsData";
 
 const ContactsPanel = () => {
     const currentTime = useCurrentTime("Europe/Kyiv");
+
+    const renderItem = (item: ContactTypes) => {
+        if (item.id === "time") {
+            return (
+                <time className="contacts-panel__menu-time">
+                    <span className="contacts-panel__menu-icon">{item.icon}</span>
+                    {currentTime}
+                </time>
+            );
+        }
+
+        if (item.link) {
+            return (
+                <a
+                    href={item.link}
+                    className="contacts-panel__menu-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <span className="contacts-panel__menu-icon">{item.icon}</span>
+                    {item.text}
+                </a>
+            );
+        }
+
+        return (
+            <p className="contacts-panel__menu-text">
+                <span className="contacts-panel__menu-icon">{item.icon}</span>
+                {item.text}
+            </p>
+        );
+    };
 
     return (
         <>
             <aside className="contacts-panel">
                 <nav className="contacts-panel__menu">
                     <ul className="contacts-panel__menu-list">
-                        {contactData.map((item, index) => (
-                            <li key={index} className="contacts-panel__menu-item">
-                                {item.id === "time" ? (
-                                    <time className="contacts-panel__menu-time">
-                                        <span className="contacts-panel__menu-icon">{item.icon}</span>
-                                        {currentTime}
-                                    </time>
-                                ) : item.link ? (
-                                    <a
-                                        href={item.link}
-                                        className="contacts-panel__menu-link"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <span className="contacts-panel__menu-icon">{item.icon}</span>
-                                        {item.text}
-                                    </a>
-                                ) : (
-                                    <p className="contacts-panel__menu-text">
-                                        <span className="contacts-panel__menu-icon">{item.icon}</span>
-                                        {item.text}
-                                    </p>
-                                )}
+                        {contactsData.map((item, index) => (
+                            <li key={index} className="contacts-panel__menu-item" id={item.id || undefined}>
+                                {renderItem(item)}
                             </li>
                         ))}
                     </ul>
---
+++ src/types/contactTypes.ts
@@ -0,0 +1,6 @@
+export interface ContactTypes {
+    id: string;
+    icon: JSX.Element;
+    text: string;
+    link?: string;
+}
