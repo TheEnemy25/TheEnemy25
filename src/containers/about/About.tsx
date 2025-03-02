d2beb15ac14516a5da0cf522ffeff9a838e5616b
TheEnemy25 <andrikkobrin@gmail.com>
2025-03-02 19:54:15 +0200

    Revert "feat(About): Added id for spy scroll on section"
    This reverts commit 6952007568f54e0e1ad52ccd925826dbe643746a.

--- src/containers/about/About.tsx
+++ src/containers/about/About.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import "./About.scss";
 import photo from "/images/photo.png";
-import { Link } from "react-router-dom";
 
 const About: React.FC = () => {
 
     return (
         <>
-            <section id="about" className="about">
+            <section className="about">
                 <article className="about__container">
                     <header className="about__header">
-                        <h1 className="about__header-title"><Link to=""></Link>Andrian Kobryn</h1>
+                        <h1 className="about__header-title">Andrian Kobryn</h1>
                         <h2 className="about__header-subtitle">.NET Developer</h2>
                     </header>
 
