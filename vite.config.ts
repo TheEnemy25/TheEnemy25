e2bf7c3155405fccc3de05c241c5eecf9b07cd65
TheEnemy25 <andrikkobrin@gmail.com>
2025-04-02 17:34:03 +0300

    fix: Corrected base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/portfolio-kobryn/",
   plugins: [react()],
-  server: {
-    port: 3000,
+  build: {
+    outDir: "dist",
   },
 })
