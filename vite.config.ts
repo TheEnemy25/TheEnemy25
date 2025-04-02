145428855f2b9b942d33d3ecd3dc12067d3c9ed7
TheEnemy25 <andrikkobrin@gmail.com>
2025-04-02 17:21:52 +0300

    fix: Fixed fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/portfolio-kobryn/",
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
   },
