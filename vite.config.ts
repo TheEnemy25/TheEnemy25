1fa868308676a29b8890ec8f4cd8ae58df779514
TheEnemy25 <andrikkobrin@gmail.com>
2025-09-07 02:42:57 +0300

    feat(data): Removed unnecessary prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const basePath = process.env.NODE_ENV === 'production' ? '/TheEnemy25/' : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/TheEnemy25/",
+  base: basePath,
   plugins: [react()],
   build: {
     outDir: "dist",
